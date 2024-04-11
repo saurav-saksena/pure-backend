d4ef0994fff6dbf26b7cbf974d549d26f92518a5
saurav-saksena <sauravkumar4363@gmail.com>
2024-04-11 09:32:31 +0530

    database connection completed

--- Readme.md
+++ Readme.md
@@ -1,1 +1,5 @@
 # A complete backend project to learn everything about backend with javaScript
+
+### A JavaScript IIFE (Immediately Invoked Function Expression) is a function that runs the moment it is invoked or called in the JavaScript event loop. Having a function that behaves that way can be useful for several use cases.
+
+### process.exit(1) in node js
--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
+import connectDB from "./database/index.js";
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./env",
+});
+connectDB();
 
+/*
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import express from "express";
+const app = express()(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERR:", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `app is listening on port http://localhost:${process.env.PORT}`
+      );
+    });
+  } catch (error) {
+    console.log("ERROR : ", error);
+    throw error;
+  }
+})();
+*/
